15fa21226e85a669c79a05294a01eb24d6915ff0
greensnapback0229 <smdmim@gmail.com>
2024-07-15 14:15:24 +0900

    init: 프로젝트 초기 설정

---
+++ src/main/java/comatching/comatching3/Comatching3Application.java
@@ -0,0 +1,15 @@
+package comatching.comatching3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
+@SpringBootApplication
+@EnableJpaAuditing
+public class Comatching3Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Comatching3Application.class, args);
+	}
+
+}
---
+++ src/test/java/comatching/comatching3/Comatching3ApplicationTests.java
@@ -0,0 +1,13 @@
+package comatching.comatching3;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Comatching3ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
