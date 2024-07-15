21aeb19cc24afa180769cc30fb727c54b5daed8a
greensnapback0229 <smdmim@gmail.com>
2024-07-15 14:16:21 +0900

    init: Users, UserAiFeature 엔티티 추가

---
+++ src/main/java/comatching/comatching3/Users/entity/UserAiFeature.java
@@ -0,0 +1,54 @@
+package comatching.comatching3.Users.entity;
+
+import java.util.List;
+
+import comatching.comatching3.Users.enums.ContactFrequency;
+import comatching.comatching3.Users.enums.Gender;
+import comatching.comatching3.Users.enums.Hobby;
+import comatching.comatching3.Users.enums.Major;
+import comatching.comatching3.util.HobbyListConverter;
+import jakarta.persistence.Column;
+import jakarta.persistence.Convert;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.OneToOne;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class UserAiFeature {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name = "user_ai_feature_id")
+	private Long id;
+
+
+	@OneToOne(fetch = FetchType.LAZY)
+	@JoinColumn(name = "users_id")
+	private Users users;
+
+	private String mbti;
+
+	@Enumerated(EnumType.STRING)
+	private ContactFrequency contactFrequency;
+
+	@Convert(converter = HobbyListConverter.class)
+	private List<Hobby> hobby;
+
+	private Integer age;
+
+	@Enumerated(EnumType.STRING)
+	private Gender gender;
+
+	@Enumerated(EnumType.STRING)
+	private Major major;
+}
---
+++ src/main/java/comatching/comatching3/Users/entity/Users.java
@@ -0,0 +1,71 @@
+package comatching.comatching3.Users.entity;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import comatching.comatching3.history.entity.PointHistory;
+import comatching.comatching3.match_message.entity.MessageMap;
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.OneToOne;
+import jakarta.persistence.Table;
+import jakarta.persistence.UniqueConstraint;
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Table(name = "users", uniqueConstraints = {
+	@UniqueConstraint(
+		name = "social_id_unique",
+		columnNames = "social_id"
+	)
+})
+public class Users {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name = "users_id")
+	private Long id;
+
+	@OneToOne(mappedBy = "users", fetch = FetchType.LAZY, cascade = CascadeType.ALL, orphanRemoval = true)
+	private UserAiFeature userAiFeature;
+
+	@OneToMany(mappedBy = "userInfo", fetch = FetchType.LAZY)
+	private List<PointHistory> pointHistoryList = new ArrayList<PointHistory>();
+
+	@OneToMany(mappedBy = "sender", cascade = CascadeType.ALL, orphanRemoval = true)
+	private List<MessageMap> sentMessageMap = new ArrayList<MessageMap>();
+
+	@OneToMany(mappedBy = "receiver", cascade = CascadeType.ALL, orphanRemoval = true)
+	private List<MessageMap> receivedMessageMap = new ArrayList<MessageMap>();
+
+	private String socialId;
+
+	private String contactId;
+
+	private String email;
+
+	private String role;
+
+	private Integer pickMe;
+
+	private String song;
+
+	private Integer point;
+
+	private String comment;
+
+
+
+
+}
