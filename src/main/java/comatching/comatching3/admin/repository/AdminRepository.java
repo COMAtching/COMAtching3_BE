fad3894ae32828e84a4995adc0ad2d3a23b75267
popeye0618 <popeye0618@gmail.com>
2024-07-18 21:23:42 +0900

    chore : 관리자 repository 추가

---
+++ src/main/java/comatching/comatching3/admin/repository/AdminRepository.java
@@ -0,0 +1,7 @@
+package comatching.comatching3.admin.repository;
+
+import comatching.comatching3.admin.entity.Admin;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface AdminRepository extends JpaRepository<Admin, Long> {
+}
