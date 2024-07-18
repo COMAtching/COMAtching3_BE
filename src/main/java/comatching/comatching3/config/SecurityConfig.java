9e772a988f8221506bc6308935041b9d8197ccb5
popeye0618 <popeye0618@gmail.com>
2024-07-18 21:07:34 +0900

    feat : 시큐리티 설정 및 oauth 추가

---
+++ src/main/java/comatching/comatching3/config/SecurityConfig.java
@@ -0,0 +1,85 @@
+package comatching.comatching3.config;
+
+import comatching.comatching3.users.oauth2.handler.OAuth2SuccessHandler;
+import comatching.comatching3.users.oauth2.service.CustomOAuth2UserService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.oauth2.client.web.OAuth2LoginAuthenticationFilter;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+import java.util.Collections;
+
+@Configuration
+@EnableWebSecurity
+@RequiredArgsConstructor
+public class SecurityConfig {
+
+    private final CustomOAuth2UserService customOAuth2UserService;
+    private final OAuth2SuccessHandler oAuth2SuccessHandler;
+
+
+    @Bean
+    public WebSecurityCustomizer webSecurityCustomizer() { // security를 적용하지 않을 리소스
+        return web -> web.ignoring()
+                .requestMatchers("/error", "/favicon.ico");
+    }
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http
+                .cors(corsCustomizer -> corsCustomizer.configurationSource(corsConfigurationSource()));
+
+        http
+                .csrf(AbstractHttpConfigurer::disable)
+                .formLogin(AbstractHttpConfigurer::disable)
+                .httpBasic(AbstractHttpConfigurer::disable);
+
+//        http
+//                .addFilterAfter(new JwtFilter(jwtUtil, userService), OAuth2LoginAuthenticationFilter.class);
+
+        http
+                .oauth2Login(oauth2 -> oauth2
+                        .userInfoEndpoint(userInfoEndpointConfig -> userInfoEndpointConfig
+                                .userService(customOAuth2UserService))
+                        .successHandler(oAuth2SuccessHandler)
+                );
+
+        http
+                .authorizeHttpRequests(auth -> auth
+                        .requestMatchers("/login-form").permitAll()
+                        .requestMatchers("/admin/**").hasRole("admin")
+                        .requestMatchers("/social/**").hasRole("social")
+                        .anyRequest().authenticated()
+                );
+
+        http
+                .sessionManagement((session) -> session
+                        .sessionCreationPolicy(SessionCreationPolicy.STATELESS));
+
+        return http.build();
+    }
+
+    private CorsConfigurationSource corsConfigurationSource() {
+        CorsConfiguration configuration = new CorsConfiguration();
+        configuration.setAllowedOriginPatterns(Collections.singletonList("*"));
+        configuration.setAllowedMethods(Collections.singletonList("*"));
+        configuration.setAllowCredentials(true);
+        configuration.setAllowedHeaders(Collections.singletonList("*"));
+        configuration.setExposedHeaders(Collections.singletonList("Authorization"));
+        configuration.setMaxAge(3600L);
+
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", configuration);
+
+        return source;
+    }
+}
---
+++ src/main/java/comatching/comatching3/users/oauth2/handler/OAuth2SuccessHandler.java
@@ -0,0 +1,6 @@
+package comatching.comatching3.users.oauth2.handler;
+
+import org.springframework.security.web.authentication.SimpleUrlAuthenticationSuccessHandler;
+
+public class OAuth2SuccessHandler extends SimpleUrlAuthenticationSuccessHandler {
+}
