25319e67560795c0b352bc3520e0b1f218e2ce98
popeye0618 <popeye0618@gmail.com>
2024-07-18 21:09:35 +0900

    feat : 시큐리티 설정 및 oauth 추가

--- src/main/java/comatching/comatching3/users/oauth2/handler/OAuth2SuccessHandler.java
+++ src/main/java/comatching/comatching3/users/oauth2/handler/OAuth2SuccessHandler.java
@@ -1,6 +1,10 @@
 package comatching.comatching3.users.oauth2.handler;
 
+import lombok.RequiredArgsConstructor;
 import org.springframework.security.web.authentication.SimpleUrlAuthenticationSuccessHandler;
+import org.springframework.stereotype.Component;
 
+@Component
+@RequiredArgsConstructor
 public class OAuth2SuccessHandler extends SimpleUrlAuthenticationSuccessHandler {
 }
