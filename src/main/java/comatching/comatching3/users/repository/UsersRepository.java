0bdce0531fd3446a06806034d042554881f335ac
popeye0618 <popeye0618@gmail.com>
2024-07-22 12:36:41 +0900

    CMT-21 chore: 레포지토리 메소드 Optional로 변경

--- src/main/java/comatching/comatching3/users/repository/UsersRepository.java
+++ src/main/java/comatching/comatching3/users/repository/UsersRepository.java
@@ -3,5 +3,9 @@ package comatching.comatching3.users.repository;
 import comatching.comatching3.users.entity.Users;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface UsersRepository extends JpaRepository<Users, Long> {
+
+    Optional<Users> findBySocialId(String socialId);
 }
