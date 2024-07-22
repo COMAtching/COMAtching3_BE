dd3559e3c1b7dec009527180678efac000605cc4
popeye0618 <popeye0618@gmail.com>
2024-07-23 08:25:29 +0900

    CMT-21 feat: 중복 ID에 관한 사용자 설정 예외 추가

---
+++ src/main/java/comatching/comatching3/admin/exception/AccountIdAlreadyExistsException.java
@@ -0,0 +1,7 @@
+package comatching.comatching3.admin.exception;
+
+public class AccountIdAlreadyExistsException extends RuntimeException {
+    public AccountIdAlreadyExistsException(String message) {
+        super(message);
+    }
+}
