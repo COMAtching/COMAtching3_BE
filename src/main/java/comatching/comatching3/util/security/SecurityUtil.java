0f86d90a784812d5c9e4d5b984e5548f9231d345
greensnapback0229 <smdmim@gmail.com>
2024-08-09 17:34:49 +0900

    CMT-22 feat: securityUtil 현재 사용자 가져오는 메서드 추가

--- src/main/java/comatching/comatching3/util/security/SecurityUtil.java
+++ src/main/java/comatching/comatching3/util/security/SecurityUtil.java
@@ -1,12 +1,29 @@
 package comatching.comatching3.util.security;
 
+import org.hibernate.validator.constraints.UUID;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.stereotype.Component;
 
 import java.util.Optional;
 
+import comatching.comatching3.exception.BusinessException;
+import comatching.comatching3.users.entity.Users;
+import comatching.comatching3.users.repository.UsersRepository;
+import comatching.comatching3.util.ResponseCode;
+import comatching.comatching3.util.UUIDUtil;
+
+@Component
 public class SecurityUtil {
+
+    private final UsersRepository usersRepository;
+
+    public SecurityUtil(UsersRepository usersRepository){
+        this.usersRepository = usersRepository;
+    }
 
     /**
      *
@@ -43,5 +60,23 @@ public class SecurityUtil {
                     .getAuthority();
         }
         return null;
+    }
+
+    /**
+     *
+     * @return 현재 Users Entity 반환
+     */
+    public Users getCurrentUsersEntity(){
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+
+        if (authentication != null && authentication.getPrincipal() instanceof UserDetails) {
+            UserDetails userDetails =  (UserDetails) authentication.getPrincipal();
+            byte[] uuid = UUIDUtil.uuidStringToBytes(userDetails.getUsername());
+
+            return usersRepository.findUsersByUuid(uuid)
+                .orElseThrow(() -> new BusinessException(ResponseCode.USER_NOT_FOUND));
+        }
+
+        throw new BusinessException(ResponseCode.USER_NOT_FOUND);
     }
 }
