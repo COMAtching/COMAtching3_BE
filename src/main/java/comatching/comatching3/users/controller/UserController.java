90ca229d634b93e86709d8e33255cacefecd7035
popeye0618 <popeye0618@gmail.com>
2024-08-14 12:44:42 +0900

    CMT-26 chore : 엔드포인트 수정

--- src/main/java/comatching/comatching3/users/controller/UserController.java
+++ src/main/java/comatching/comatching3/users/controller/UserController.java
@@ -21,7 +21,7 @@ public class UserController {
      * @param form social 유저의 Feature
      * @return 처리 결과 반환
      */
-    @PostMapping("/social/api/user/input-info")
+    @PostMapping("/social/api/user/info")
     public Response<?> inputUserInfo(@RequestBody UserFeatureReq form) {
         userService.inputUserInfo(form);
         return Response.ok();
@@ -31,7 +31,7 @@ public class UserController {
      * 메인 페이지 유저 정보 조회
      * @return 유저 정보
      */
-    @GetMapping("/api/user/info")
+    @GetMapping("/user/api/info")
     public Response<?> getUserInfo() {
         UserInfoRes userInfo = userService.getUserInfo();
         return Response.ok(userInfo);
@@ -41,7 +41,7 @@ public class UserController {
      * 유저 포인트 조회
      * @return 유저 포인트
      */
-    @GetMapping("/api/user/points")
+    @GetMapping("/user/api/points")
     public Response<?> getPoints() {
         Integer points = userService.getPoints();
         return Response.ok("point : " + points);
