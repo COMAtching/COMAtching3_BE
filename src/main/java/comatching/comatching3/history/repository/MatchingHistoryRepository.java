6c0760858980f464fb4d2c776fd96fd0bdf8ecbe
greensnapback0229 <smdmim@gmail.com>
2024-08-27 00:48:05 +0900

    CMT-22 feat: 매칭 내역 조회 추가

--- src/main/java/comatching/comatching3/history/repository/MatchingHistoryRepository.java
+++ src/main/java/comatching/comatching3/history/repository/MatchingHistoryRepository.java
@@ -4,6 +4,9 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 import comatching.comatching3.history.entity.MatchingHistory;
 
+import java.util.List;
+
 public interface MatchingHistoryRepository extends JpaRepository<MatchingHistory, Long> {
 
+    List<MatchingHistory> findMatchingHistoriesByApplierId(Long applierId);
 }
