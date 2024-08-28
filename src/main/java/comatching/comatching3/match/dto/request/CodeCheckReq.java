86c31aaf3eea9c0c597bd57f011c1e74bab1d620
greensnapback0229 <smdmim@gmail.com>
2024-08-29 03:37:17 +0900

    CMT-22 feat: 매칭 코드 인증 기능 추가

---
+++ src/main/java/comatching/comatching3/match/dto/request/CodeCheckReq.java
@@ -0,0 +1,11 @@
+package comatching.comatching3.match.dto.request;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor
+public class CodeCheckReq {
+
+	private String code;
+}
