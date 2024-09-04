688a4ac8d5d14e83f389e862543bd7b473a43417
greensnapback0229 <smdmim@gmail.com>
2024-09-04 14:53:26 +0900

    feat: AI 서버 응답 포맷 변경 반영

--- src/main/java/comatching/comatching3/match/dto/messageQueue/MatchRequestMsg.java
+++ src/main/java/comatching/comatching3/match/dto/messageQueue/MatchRequestMsg.java
@@ -1,17 +1,33 @@
 package comatching.comatching3.match.dto.messageQueue;
 
 import comatching.comatching3.match.dto.request.MatchReq;
+import comatching.comatching3.match.enums.AgeOption;
+import comatching.comatching3.match.enums.ContactFrequencyOption;
+import comatching.comatching3.users.enums.Hobby;
 import lombok.Getter;
 
 @Getter
-public class MatchRequestMsg extends MatchReq {
+public class MatchRequestMsg{
 
-	public MatchRequestMsg(MatchReq matchReq){
-		this.mbti = matchReq.getMbti();
-		this.ageOption = matchReq.getAgeOption();
-		this.uuid = matchReq.getUuid();
-		this.hobbyOption = matchReq.getHobbyOption();
+	private String matcherUuid;
+	private ContactFrequencyOption contactFrequencyOption;
+	private String genderOption;
+	private String hobbyOption;
+	private Boolean sameMajorOption;
+	private AgeOption ageOption;
+	private String mbtiOption;
+	private String myMajor;
+	private Integer myAge;
+
+	public void fromMatchReq(MatchReq matchReq){
+		this.matcherUuid = matchReq.getUuid();
 		this.contactFrequencyOption = matchReq.getContactFrequencyOption();
+		this.genderOption = "FEMALE";
+		this.hobbyOption = Hobby.convertHobbiesString(matchReq.getHobbyOption());
 		this.sameMajorOption = matchReq.getSameMajorOption();
+		this.ageOption = matchReq.getAgeOption();
+		this.mbtiOption = matchReq.getMbti();
+		this.myMajor = "정보통신전자공학부";
+		this.myAge = 24;
 	}
 }
