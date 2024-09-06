d76fa71399484cbda6a781a9609b4768b1d4e0dc
popeye0618 <popeye0618@gmail.com>
2024-09-06 17:01:33 +0900

    chore : 자잘한 수정

--- src/main/java/comatching/comatching3/config/WebSocketConfig.java
+++ src/main/java/comatching/comatching3/config/WebSocketConfig.java
@@ -31,7 +31,7 @@ import java.util.Map;
 public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
 
     private final JwtUtil jwtUtil;
-    private final SimpMessagingTemplate simpMessagingTemplate;
+//    private final SimpMessagingTemplate simpMessagingTemplate;
 
     @Override
     public void registerStompEndpoints(StompEndpointRegistry registry) {
