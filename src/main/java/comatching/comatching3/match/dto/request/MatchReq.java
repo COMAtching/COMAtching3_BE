89eed9b75c73e49eb01ec51f9dd34cf341f6d21c
greensnapback0229 <smdmim@gmail.com>
2024-09-10 03:34:25 +0900

    chore: 매칭 요청 dto 불필요 필드 제거

--- src/main/java/comatching/comatching3/match/dto/request/MatchReq.java
+++ src/main/java/comatching/comatching3/match/dto/request/MatchReq.java
@@ -6,7 +6,6 @@ import comatching.comatching3.match.enums.AgeOption;
 import comatching.comatching3.match.enums.ContactFrequencyOption;
 import comatching.comatching3.users.enums.Hobby;
 import comatching.comatching3.util.validation.ValidEnum;
-import jakarta.validation.constraints.Pattern;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
@@ -17,18 +16,15 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 @AllArgsConstructor
 public class MatchReq {
-	@Pattern(regexp = "^[0-9a-fA-F]{32}$", message = "Invalid UUID format")
-	protected String uuid;
-
 	@ValidEnum(enumClass = AgeOption.class)
-	protected AgeOption ageOption;
-	protected String mbtiOption;
+	private AgeOption ageOption;
+	private String mbtiOption;
 
 	@ValidEnum(enumClass =  Hobby.class)
-	protected List<Hobby> hobbyOption;
+	private List<Hobby> hobbyOption;
 
 	@ValidEnum(enumClass = ContactFrequencyOption.class)
-	protected ContactFrequencyOption contactFrequencyOption;
-	protected Boolean sameMajorOption;
-	protected String duplicationList;
+	private ContactFrequencyOption contactFrequencyOption;
+	private Boolean sameMajorOption;
+	private String duplicationList;
 }
