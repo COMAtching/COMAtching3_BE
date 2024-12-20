b60fe2885dc02d5c607cc42eb0237dd111c23f60
popeye0618 <popeye0618@gmail.com>
2024-12-20 17:09:38 +0900

    fix: JWT 로직 개선
    RT 적용 완료 및 RTR 적용 완료
    쿠키 사용 방식으로 변경

--- src/main/java/comatching/comatching3/users/auth/jwt/JwtFilter.java
+++ src/main/java/comatching/comatching3/users/auth/jwt/JwtFilter.java
@@ -1,17 +1,27 @@
 package comatching.comatching3.users.auth.jwt;
 
 import java.io.IOException;
+import java.util.Arrays;
 import java.util.List;
 
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.filter.OncePerRequestFilter;
 
+import comatching.comatching3.admin.auth.AdminDto;
+import comatching.comatching3.admin.auth.CustomAdmin;
+import comatching.comatching3.admin.enums.AdminRole;
+import comatching.comatching3.users.auth.oauth2.dto.UserDto;
+import comatching.comatching3.users.auth.oauth2.provider.CustomUser;
 import comatching.comatching3.users.auth.refresh_token.service.RefreshTokenService;
-import comatching.comatching3.util.security.SecurityUtil;
+import comatching.comatching3.util.CookieUtil;
 import io.jsonwebtoken.ExpiredJwtException;
 import io.jsonwebtoken.JwtException;
 import io.jsonwebtoken.security.SignatureException;
 import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
+import jakarta.servlet.http.Cookie;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
@@ -25,11 +35,12 @@ public class JwtFilter extends OncePerRequestFilter {
 		"/login",
 		"/api/match",
 		"/charge-monitor",
-		"/admin"
+		"/admin",
+		"/auth/refresh"
 	);
 	private final JwtUtil jwtUtil;
 	private final RefreshTokenService refreshTokenService;
-	private final SecurityUtil securityUtil;
+	private final CookieUtil cookieUtil;
 
 	@Override
 	protected void doFilterInternal(HttpServletRequest request,
@@ -37,101 +48,84 @@ public class JwtFilter extends OncePerRequestFilter {
 		FilterChain filterChain) throws ServletException, IOException {
 
 		String requestURI = request.getRequestURI();
-		//        log.info("요청 URL = {}", requestURI);
+		log.info("요청 URL = {}", requestURI);
 		if (WHITELIST.stream().anyMatch(requestURI::startsWith)) {
 			filterChain.doFilter(request, response);
 			return;
 		}
 
-		setSecurityHeaders(response);
-
-		String accessToken = getAccessToken(request);
+		String encryptedAccessToken = getTokenFromCookies(request, "accessToken");
 
 		try {
-			if (accessToken != null && !jwtUtil.isExpired(accessToken)) {
-				//                log.info("엑세스 토큰 유효");
-				securityUtil.setAuthentication(accessToken);
-				filterChain.doFilter(request, response);
-				return;
+			if (encryptedAccessToken != null) {
+				// 토큰 복호화
+				String accessToken = jwtUtil.decryptToken(encryptedAccessToken);
+
+				if (!jwtUtil.isExpired(accessToken)) {
+					// 복호화된 토큰 유효성 검증
+					log.info("액세스 토큰 사용");
+					setAuthentication(accessToken);
+					filterChain.doFilter(request, response);
+					return;
+				}
 			}
 		} catch (ExpiredJwtException e) {
+			log.info("엑세스 토큰 만료");
 			throw new JwtException("TOKEN_EXPIRED");
-			//            log.info("엑세스 토큰 만료");
 		} catch (SignatureException e) {
 			log.info("엑세스 토큰 무결성 오류");
 			throw new JwtException("TOKEN_INVALID");
 		} catch (JwtException e) {
 			log.info("엑세스 토큰 오류");
-			//            response.sendRedirect("/login");
-			throw new JwtException("TOKEN_INVALID");
-		}
-
-		String refreshToken = getRefreshToken(request);
-
-		try {
-			if (refreshToken != null && !jwtUtil.isExpired(refreshToken)) {
-				//                log.info("헤더 리프레시 토큰 유효");
-				String socialId = jwtUtil.getUUID(refreshToken);
-				String role = jwtUtil.getRole(refreshToken);
-				String redisRefreshToken = refreshTokenService.getRefreshToken(socialId);
-
-				if (redisRefreshToken != null && redisRefreshToken.equals(refreshToken)) {
-					//                    log.info("레디스 리프레시 토큰까지 유효, 엑세스 토큰 재발급");
-					String newAccessToken = jwtUtil.generateAccessToken(socialId, role);
-					//                    log.info("새 엑세스 토큰 출력 = {}", newAccessToken);
-
-					//                    log.info("리프레시 토큰을 사용했으므로 재발급");
-					String newRefreshToken = jwtUtil.generateRefreshToken(socialId, role);
-					//                    log.info("새 리프레시 토큰 출력 = {}", newRefreshToken);
-					refreshTokenService.saveRefreshToken(socialId, newRefreshToken);
-
-					response.setHeader("Authorization", "Bearer " + newAccessToken);
-					response.setHeader("Refresh-Token", newRefreshToken);
-					securityUtil.setAuthentication(newAccessToken);
-					//                    Users user = securityUtil.getCurrentUsersEntity();
-					//                    log.info("새로 등록한 유저 정보 출력(필터 안) " + user.getUsername() + "권한 " + user.getRole());
-				} else {
-					//                    log.info("레디스와 리프레시 토큰 다름");
-					//                    response.sendRedirect("/login");
-					throw new JwtException("TOKEN_INVALID");
-				}
-			}
-		} catch (ExpiredJwtException e) {
-			//            log.info("리프레시 토큰 만료");
-			//            response.sendRedirect("/login");
-			throw new JwtException("TOKEN_EXPIRED");
-		} catch (SignatureException e) {
-			//            log.info("리프레시 토큰 무결성 오류");
-			throw new JwtException("TOKEN_INVALID");
-		} catch (JwtException e) {
-			//            log.info("리프레시 토큰 오류");
 			throw new JwtException("TOKEN_INVALID");
 		}
 
 		filterChain.doFilter(request, response);
 	}
 
-	private String getAccessToken(HttpServletRequest request) {
-		String bearerToken = request.getHeader("Authorization");
+	private void setAuthentication(String accessToken) {
+		String uuid = jwtUtil.getUUID(accessToken);
+		String role = jwtUtil.getRole(accessToken);
+		log.info("role = " + role);
 
-		if (bearerToken != null && bearerToken.startsWith("Bearer ")) {
-			//            log.info("엑세스 토큰 출력 = {}", bearerToken.substring(7));
-			return bearerToken.substring(7);
+		if (AdminRole.isValidRole(role)) {
+			AdminDto adminDto = AdminDto.builder()
+				.uuid(uuid)
+				.role(role)
+				.build();
+
+			CustomAdmin adminUser = new CustomAdmin(adminDto);
+			Authentication authToken = new UsernamePasswordAuthenticationToken(adminUser, null, adminUser.getAuthorities());
+			SecurityContextHolder.getContext().setAuthentication(authToken);
+
+		} else {
+			UserDto userDto = UserDto.builder()
+				.uuid(uuid)
+				.role(role)
+				.build();
+
+			CustomUser customUser = new CustomUser(userDto);
+
+			Authentication authToken = new UsernamePasswordAuthenticationToken(
+				customUser,
+				null,
+				customUser.getAuthorities()
+			);
+
+			SecurityContextHolder.getContext().setAuthentication(authToken);
 		}
 
-		return null;
 	}
 
-	private String getRefreshToken(HttpServletRequest request) {
-		//        log.info("리프레시 토큰 출력 = {}", request.getHeader("Refresh-Token"));
-		return request.getHeader("Refresh-Token");
-	}
+	private String getTokenFromCookies(HttpServletRequest request, String cookieName) {
+		if (request.getCookies() == null) {
+			return null;
+		}
 
-	private void setSecurityHeaders(HttpServletResponse response) {
-		response.setHeader("X-Content-Type-Options", "nosniff"); //브라우저가 MIME 타입을 스니핑하지 못하도록 설정
-		//        response.setHeader("X-Frame-Options", "DENY"); //페이지가 iframe 또는 프레임에 삽입되지 않도록 설정하여 Clickjacking 공격을 방지
-		//        response.setHeader("Strict-Transport-Security", "max-age=31536000; includeSubDomains"); //모든 연결이 HTTPS를 통해 이루어지도록 강제
-		//        response.setHeader("Content-Security-Policy", "default-src 'self'; script-src 'self' https://trusted-cdn.com");
+		return Arrays.stream(request.getCookies())
+			.filter(cookie -> cookie.getName().equals(cookieName))
+			.findFirst()
+			.map(Cookie::getValue)
+			.orElse(null);
 	}
-
 }
--- src/main/java/comatching/comatching3/users/auth/refresh_token/service/RefreshTokenService.java
+++ src/main/java/comatching/comatching3/users/auth/refresh_token/service/RefreshTokenService.java
@@ -1,30 +1,54 @@
 package comatching.comatching3.users.auth.refresh_token.service;
 
+import comatching.comatching3.admin.entity.Admin;
+import comatching.comatching3.admin.repository.AdminRepository;
+import comatching.comatching3.exception.BusinessException;
+import comatching.comatching3.users.entity.UserAiFeature;
+import comatching.comatching3.users.repository.UserAiFeatureRepository;
+import comatching.comatching3.users.repository.UsersRepository;
+import comatching.comatching3.util.ResponseCode;
+import comatching.comatching3.util.UUIDUtil;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
+import java.util.List;
+import java.util.Objects;
+import java.util.Optional;
+import java.util.Set;
 import java.util.concurrent.TimeUnit;
+import java.util.stream.Collectors;
 
-import static comatching.comatching3.users.auth.jwt.JwtUtil.REFRESH_TOKEN_EXPIRATION;
+import static comatching.comatching3.users.auth.jwt.JwtExpirationConst.*;
 
 
+@Slf4j
 @Service
 @RequiredArgsConstructor
 public class RefreshTokenService {
 
     private final RedisTemplate<String, Object> redisTemplate;
 
+    private static final String REFRESH_TOKEN_PREFIX = "refreshToken:";
 
-    public void saveRefreshToken(String uuid, String refreshToken) {
-        redisTemplate.opsForValue().set(uuid, refreshToken, REFRESH_TOKEN_EXPIRATION, TimeUnit.MILLISECONDS);
+    @Transactional
+    public void saveRefreshTokenInRedis(String uuid, String refreshToken) {
+        String key = REFRESH_TOKEN_PREFIX + uuid;
+        redisTemplate.opsForValue().set(key, refreshToken, REFRESH_TOKEN_EXPIRATION, TimeUnit.MILLISECONDS);
+
+        log.info("refreshToken 저장 완료!, key = " + key);
     }
 
     public String getRefreshToken(String uuid) {
-        return (String) redisTemplate.opsForValue().get(uuid);
+        String key = REFRESH_TOKEN_PREFIX + uuid;
+        return (String) redisTemplate.opsForValue().get(key);
     }
 
     public void deleteRefreshToken(String uuid) {
-        redisTemplate.delete(uuid);
+        String key = REFRESH_TOKEN_PREFIX + uuid;
+        redisTemplate.delete(key);
     }
 }
