06a0732a5db47e58233b9c4941a001a223d3137b
popeye0618 <popeye0618@gmail.com>
2025-01-12 16:49:25 +0900

    feat($admin): DTO Validated 설정

--- src/main/java/comatching/comatching3/admin/dto/request/UniversityReq.java
+++ src/main/java/comatching/comatching3/admin/dto/request/UniversityReq.java
@@ -1,10 +1,15 @@
 package comatching.comatching3.admin.dto.request;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import lombok.Getter;
 
 @Getter
 public class UniversityReq {
+
+    @NotNull @NotBlank
     private String name;
+
+    @NotNull @NotBlank
     private String mailDomain;
-    private String appName;
 }
