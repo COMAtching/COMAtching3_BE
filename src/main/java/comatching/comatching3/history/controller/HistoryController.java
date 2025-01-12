163920bcbe8deb41b38fe9bed04427c4184abe0a
popeye0618 <popeye0618@gmail.com>
2025-01-12 16:57:21 +0900

    feat($history): 내역 관련 기능 추가, 변경

--- src/main/java/comatching/comatching3/history/controller/HistoryController.java
+++ src/main/java/comatching/comatching3/history/controller/HistoryController.java
@@ -5,13 +5,14 @@ import java.util.List;
 import comatching.comatching3.history.dto.res.PointHistoryRes;
 
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 import comatching.comatching3.history.dto.res.MatchHistoryRes;
 import comatching.comatching3.history.service.HistoryService;
+import comatching.comatching3.pay.dto.res.PayHistoryRes;
+import comatching.comatching3.pay.service.PayHistoryService;
 import comatching.comatching3.users.entity.Users;
-import comatching.comatching3.users.service.UserService;
 import comatching.comatching3.util.Response;
 import comatching.comatching3.util.UUIDUtil;
 import comatching.comatching3.util.security.SecurityUtil;
@@ -21,6 +22,7 @@ import lombok.RequiredArgsConstructor;
 @RequiredArgsConstructor
 public class HistoryController {
 	private final HistoryService historyService;
+	private final PayHistoryService payHistoryService;
 	private final SecurityUtil securityUtil;
 
 	@GetMapping("/auth/user/api/history/matching")
@@ -28,13 +30,24 @@ public class HistoryController {
 		return Response.ok(historyService.inquiryMatchHistory());
 	}
 
-	@GetMapping("/auth/admin/api/history/point")
-	public Response<List<PointHistoryRes>> getAllPointHistory(@RequestParam(name = "uuid", required = false) String uuid) {
+	/**
+	 * 관리자의 유저 포인트 내역 조회
+	 * @param uuid
+	 * @return
+	 */
+	@GetMapping("/auth/operator/api/history/point/{uuid}")
+	public Response<List<PointHistoryRes>> getAllPointHistory(@PathVariable String uuid) {
+		return Response.ok(historyService.getAllPointHistory(UUIDUtil.uuidStringToBytes(uuid)));
+	}
 
-		if (uuid != null) {
-			return Response.ok(historyService.getAllPointHistory(UUIDUtil.uuidStringToBytes(uuid)));
-		}
-		return Response.ok(historyService.getAllPointHistory());
+	/**
+	 * 관리자의 유저 결제 내역 조회
+	 * @param uuid
+	 * @return
+	 */
+	@GetMapping("/auth/operator/api/history/payment/{uuid}")
+	public Response<List<PayHistoryRes>> getAllPaymentHistory(@PathVariable String uuid) {
+		return Response.ok(payHistoryService.getPaymentHistory(UUIDUtil.uuidStringToBytes(uuid)));
 	}
 
 	@GetMapping("/auth/user/api/history/point")
