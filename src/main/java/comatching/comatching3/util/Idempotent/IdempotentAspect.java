5493c047083d52c6b01eaf735342afc81b03e803
greensnapback0229 <smdmim@gmail.com>
2025-01-15 16:10:43 +0900

    feat($Idempotent): 멱등성 관련 AOP, 어노테이션 추가

---
+++ src/main/java/comatching/comatching3/util/Idempotent/IdempotentAspect.java
@@ -0,0 +1,89 @@
+package comatching.comatching3.util.Idempotent;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import comatching.comatching3.util.RedisUtil;
+import jakarta.servlet.http.HttpServletRequest;
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.util.concurrent.TimeUnit;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Before;
+import org.aspectj.lang.annotation.Pointcut;
+import org.springframework.data.redis.core.StringRedisTemplate;
+import org.springframework.stereotype.Component;
+import org.springframework.web.context.request.RequestContextHolder;
+import org.springframework.web.context.request.ServletRequestAttributes;
+import org.springframework.web.util.ContentCachingRequestWrapper;
+
+@Slf4j
+@Aspect
+@Component
+@RequiredArgsConstructor
+public class IdempotentAspect {
+
+
+    private final StringRedisTemplate stringRedisTemplate;
+
+
+    @Pointcut("@annotation(idempotent)")
+    public void pointCut(Idempotent idempotent){
+    }
+
+    @Before(value = "pointCut(idempotent)", argNames = "joinPoint, idempotent")
+    public void before(JoinPoint joinPoint, Idempotent idempotent) throws IOException{
+        HttpServletRequest request = ((ServletRequestAttributes) RequestContextHolder.currentRequestAttributes()).getRequest();
+
+        String requestKey = getRequestKey(request);
+        String requestValue = getRequestValue(request);
+
+        log.info("[IdempotentAspect] ({}) 요청 데이터 :: {}",  requestKey, requestValue);
+
+        int expireTime = idempotent.expireTime();
+
+        Boolean isPoss = stringRedisTemplate
+                .opsForValue()
+                .setIfAbsent(requestKey, requestValue, expireTime, TimeUnit.SECONDS);
+
+        if(Boolean.FALSE.equals(isPoss)){
+            handleRequestException(requestKey, requestValue);
+        }
+
+    }
+
+    private String getRequestKey(final HttpServletRequest request){
+        String token = request.getHeader("requestKey");
+
+        if(token == null)
+            throw new IllegalArgumentException();
+
+        return token;
+    }
+
+    private String getRequestValue(final HttpServletRequest request){
+        if(!"GET".equalsIgnoreCase(request.getMethod())){
+            ContentCachingRequestWrapper cachingRequest = (ContentCachingRequestWrapper) request;
+
+            return new String(cachingRequest.getContentAsByteArray(), StandardCharsets.UTF_8);
+        }
+
+        else{
+            return request.getQueryString();
+        }
+    }
+
+    private void handleRequestException(final String requestKey, final String requestValue) {
+
+        String originRequestValue = stringRedisTemplate.opsForValue().get(requestKey);
+        log.info("[IdempotentAspect] ({}) 기존의 요청 데이터 :: {}", requestKey, originRequestValue);
+
+        if (!requestValue.isBlank() && !requestValue.equals(originRequestValue))
+            log.info("exception");
+            //Todo: exception
+        else
+            log.info("exception");
+            //Todo: exception
+    }
+}
