1a0cd9722a40e181d70e05bea3f173b88607c76e
greensnapback0229 <smdmim@gmail.com>
2025-02-25 15:18:29 +0900

    feat($Event): 이벤트 상속 정규화 및 할인 이벤트 연관관계 맵핑

---
+++ src/main/java/comatching/comatching3/admin/entity/event/DiscountEvent.java
@@ -0,0 +1,15 @@
+package comatching.comatching3.admin.entity.event;
+
+import jakarta.persistence.DiscriminatorColumn;
+import jakarta.persistence.DiscriminatorValue;
+import jakarta.persistence.Entity;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@DiscriminatorValue("DISCOUNT")
+@Getter
+public class DiscountEvent extends Event{
+    private Integer discountRate;
+}
