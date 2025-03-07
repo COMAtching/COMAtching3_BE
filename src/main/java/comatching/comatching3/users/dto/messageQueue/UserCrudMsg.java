cec8de9c0b8522e750bd40ae1cb28e8142251a08
greensnapback0229 <smdmim@gmail.com>
2025-03-08 00:53:14 +0900

    test

--- src/main/java/comatching/comatching3/users/dto/messageQueue/UserCrudMsg.java
+++ src/main/java/comatching/comatching3/users/dto/messageQueue/UserCrudMsg.java
@@ -24,6 +24,7 @@ public class UserCrudMsg {
     private String hobby;
     private String major;
     private String mbti;
+    private String duplication = "kim_seung_won_sa_rang_hae";
 
 
     public void updateFromUserAIFeatureAndType(UserCrudType type, UserAiFeature userAiFeature) {
