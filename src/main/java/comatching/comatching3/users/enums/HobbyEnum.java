e193f739eeaa6d80feb60210acb5f0d57685341c
greensnapback0229 <smdmim@gmail.com>
2025-03-20 13:04:59 +0900

    feat($Matching): Hobby 변경으로 인한 변환 관련 적용

--- src/main/java/comatching/comatching3/users/enums/HobbyEnum.java
+++ src/main/java/comatching/comatching3/users/enums/HobbyEnum.java
@@ -1,79 +1,42 @@
 package comatching.comatching3.users.enums;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
-
 import com.fasterxml.jackson.annotation.JsonCreator;
-import com.fasterxml.jackson.annotation.JsonValue;
-
 import lombok.Getter;
 
 @Getter
 public enum HobbyEnum {
-	운동(1, "운동"),
-	스포츠시청(2, "스포츠시청"),
-	요리(3, "요리"),
-	맛집탐방(4, "맛집탐방"),
-	예술관람(5, "예술관람"),
-	음악감상(6, "음악감상"),
-	악기(7, "악기"),
-	사진(8, "사진"),
-	테크(9, "테크"),
-	창작(10, "창작"),
-	야외활동(11, "야외활동"),
-	여행(12, "여행"),
-	ott시청(13, "ott시청"),
-	게임(14, "게임"),
-	독서(15, "독서"),
-	UNSELECTED(-1, "UNSELECTED");
+    운동(1, "운동"),
+    스포츠시청(2, "스포츠시청"),
+    요리(3, "요리"),
+    맛집탐방(4, "맛집탐방"),
+    예술관람(5, "예술관람"),
+    음악감상(6, "음악감상"),
+    악기(7, "악기"),
+    사진(8, "사진"),
+    테크(9, "테크"),
+    창작(10, "창작"),
+    야외활동(11, "야외활동"),
+    여행(12, "여행"),
+    ott시청(13, "ott시청"),
+    게임(14, "게임"),
+    독서(15, "독서"),
+    UNSELECTED(-1, "UNSELECTED");
 
-	private final Integer vector;
-	private final String value;
+    private final Integer vector;
+    private final String value;
 
-	HobbyEnum(Integer vector, String value) {
-		this.vector = vector;
-		this.value = value;
-	}
+    HobbyEnum(Integer vector, String value) {
+        this.vector = vector;
+        this.value = value;
+    }
 
- 	@JsonCreator
-	public static HobbyEnum from(String value) {
-		for (HobbyEnum hobbyEnum : HobbyEnum.values()) {
-			if (hobbyEnum.getValue().equals(value)) {
-				return hobbyEnum;
-			}
-		}
-		return null;
-	}
-
-	@JsonValue
-	public String getValue() {
-		return value;
-	}
-
-	public static ArrayList<String> convertHobbiesString(List<HobbyEnum> hobbies, ArrayList<String> hobbyOption) {
-
-		ArrayList<String>
-		for(HobbyEnum hobby : hobbies){
-
-		}
-
-		return hobbiesString.toString();
-	}
-	public static List<HobbyEnum> convertStringToHobbies(String hobbiesString) {
-		List<HobbyEnum> hobbiesList = new ArrayList<>();
-
-		// 문자열을 콤마로 분리
-		List<String> hobbyNames = Arrays.asList(hobbiesString.split(","));
-
-		for (String hobbyName : hobbyNames) {
-			HobbyEnum hobbyEnum = HobbyEnum.from(hobbyName);
-			if (hobbyEnum != null) {
-				hobbiesList.add(hobbyEnum);
-			}
-		}
-
-		return hobbiesList;
-	}
-
+    @JsonCreator
+    public static HobbyEnum from(String value) {
+        for (HobbyEnum hobbyEnum : HobbyEnum.values()) {
+            if (hobbyEnum.getValue().equals(value)) {
+                return hobbyEnum;
+            }
+        }
+        return null;
+    }
 }
--- src/main/java/comatching/comatching3/util/RabbitMQ/MatchRabbitMQUtil.java
+++ src/main/java/comatching/comatching3/util/RabbitMQ/MatchRabbitMQUtil.java
@@ -1,5 +1,10 @@
 package comatching.comatching3.util.RabbitMQ;
 
+import comatching.comatching3.exception.BusinessException;
+import comatching.comatching3.matching.dto.messageQueue.MatchRequestMsg;
+import comatching.comatching3.matching.dto.messageQueue.MatchResponseMsg;
+import comatching.comatching3.util.ResponseCode;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.amqp.core.MessagePostProcessor;
 import org.springframework.amqp.rabbit.connection.CorrelationData;
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
@@ -7,47 +12,43 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.core.ParameterizedTypeReference;
 import org.springframework.stereotype.Component;
 
-import comatching.comatching3.exception.BusinessException;
-import comatching.comatching3.matching.dto.messageQueue.MatchRequestMsg;
-import comatching.comatching3.matching.dto.messageQueue.MatchResponseMsg;
-import comatching.comatching3.util.ResponseCode;
-import lombok.extern.slf4j.Slf4j;
-
 @Slf4j
 @Component
 public class MatchRabbitMQUtil {
 
-	private final RabbitTemplate rabbitTemplate;
+    private final RabbitTemplate rabbitTemplate;
 
-	@Value("${rabbitmq.routing-keys.match-request}")
-	private String matchRequestQueue;
+    @Value("${rabbitmq.routing-keys.match-request}")
+    private String matchRequestQueue;
 
-	public MatchRabbitMQUtil(RabbitTemplate rabbitTemplate) {
-		this.rabbitTemplate = rabbitTemplate;
-	}
+    public MatchRabbitMQUtil(RabbitTemplate rabbitTemplate) {
+        this.rabbitTemplate = rabbitTemplate;
+    }
 
-	/**
-	 * 메세지 브로커로 매칭 요청 & 응답 처리
-	 * @param requestMsg : 매칭 리퀘스트 정보
-	 * @param requestId : 매칭 요청 고유 id
-	 * @return : 결과로 나온 유저의 uuid
-	 */
-	public MatchResponseMsg match(MatchRequestMsg requestMsg, String requestId) {
-		CorrelationData correlationData = new CorrelationData(requestId);
-		ParameterizedTypeReference<MatchResponseMsg> responseType = new ParameterizedTypeReference<MatchResponseMsg>(){};
+    /**
+     * 메세지 브로커로 매칭 요청 & 응답 처리
+     *
+     * @param requestMsg : 매칭 리퀘스트 정보
+     * @param requestId  : 매칭 요청 고유 id
+     * @return : 결과로 나온 유저의 uuid
+     */
+    public MatchResponseMsg match(MatchRequestMsg requestMsg, String requestId) {
+        CorrelationData correlationData = new CorrelationData(requestId);
+        ParameterizedTypeReference<MatchResponseMsg> responseType = new ParameterizedTypeReference<MatchResponseMsg>() {
+        };
 
-		MatchResponseMsg response = rabbitTemplate.convertSendAndReceiveAsType(
-			matchRequestQueue,
-			requestMsg,
-			(MessagePostProcessor) null,
-			correlationData,
-			responseType);
+        MatchResponseMsg response = rabbitTemplate.convertSendAndReceiveAsType(
+                matchRequestQueue,
+                requestMsg,
+                (MessagePostProcessor) null,
+                correlationData,
+                responseType);
 
-		if(response == null){
-			throw new BusinessException(ResponseCode.NO_MATCH_RESPONSE);
-		}
+        if (response == null) {
+            throw new BusinessException(ResponseCode.NO_MATCH_RESPONSE);
+        }
 
-		log.info("[MatchRabbitMQUtil] match success enemyUuid = {}", response.getEnemyUuid());
-		return response;
-	}
+        log.info("[MatchRabbitMQUtil] match success enemyUuid = {}", response.getEnemyUuid());
+        return response;
+    }
 }
