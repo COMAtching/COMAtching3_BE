1975da91cdb7a2e9da538917736c57e372459034
greensnapback0229 <smdmim@gmail.com>
2025-03-30 01:37:41 +0900

    feat($Match): 매칭 테스트 추가

--- src/main/java/comatching/comatching3/matching/service/TestService.java
+++ src/main/java/comatching/comatching3/matching/service/TestService.java
@@ -1,14 +1,22 @@
 package comatching.comatching3.matching.service;
 
 import comatching.comatching3.exception.BusinessException;
+import comatching.comatching3.matching.dto.messageQueue.MatchRequestMsg;
+import comatching.comatching3.matching.dto.request.MatchReq;
+import comatching.comatching3.matching.enums.AgeOption;
+import comatching.comatching3.matching.enums.ContactFrequencyOption;
 import comatching.comatching3.users.entity.UserAiFeature;
+import comatching.comatching3.users.enums.HobbyEnum;
 import comatching.comatching3.users.enums.UserCrudType;
 import comatching.comatching3.users.repository.UserAiFeatureRepository;
 import comatching.comatching3.users.repository.UsersRepository;
+import comatching.comatching3.util.RabbitMQ.MatchRabbitMQUtil;
 import comatching.comatching3.util.RabbitMQ.UserCrudRabbitMQUtil;
 import comatching.comatching3.util.ResponseCode;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
 
 @Service
 @RequiredArgsConstructor
@@ -17,6 +25,7 @@ public class TestService {
     private final UserCrudRabbitMQUtil userCrudRabbitMQUtil;
     private final UsersRepository usersRepository;
     private final UserAiFeatureRepository userAiFeatureRepository;
+    private final MatchRabbitMQUtil matchRabbitMQUtil;
 
     public void requestTestCrudCreate() {
         UserAiFeature userAiFeature = userAiFeatureRepository.findById(1L)
@@ -36,5 +45,18 @@ public class TestService {
         UserAiFeature userAiFeature = userAiFeatureRepository.findById(1L)
                 .orElseThrow(() -> new BusinessException(ResponseCode.MATCH_CODE_CHECK_FAIL));
         userCrudRabbitMQUtil.sendUserChange(userAiFeature, UserCrudType.UPDATE);
+    }
+
+    public void requestTestMatch() {
+        ArrayList<HobbyEnum> hobbyEnums = new ArrayList<>();
+        hobbyEnums.add(HobbyEnum.게임);
+        MatchReq matchReq = new MatchReq(AgeOption.EQUAL, "EN", hobbyEnums, ContactFrequencyOption.FREQUENT, false, "");
+
+        UserAiFeature applierFeature = userAiFeatureRepository.findById(1L)
+                .orElseThrow(() -> new BusinessException(ResponseCode.MATCH_CODE_CHECK_FAIL));
+        MatchRequestMsg msg = new MatchRequestMsg();
+        msg.fromMatchReqAndUserAiFeature(matchReq, applierFeature, "가톨릭대학교");
+
+        matchRabbitMQUtil.match(msg, "123d456d");
     }
 }
