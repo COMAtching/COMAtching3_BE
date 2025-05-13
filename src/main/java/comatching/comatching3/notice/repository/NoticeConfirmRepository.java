1e8f9bc66ab3c33299f62f3f1c2ab67bee5e47cf
greensnapback0229 <smdmim@gmail.com>
2025-05-13 15:34:48 +0900

    feat($Notice): notice 관련 jpa repository 추가

---
+++ src/main/java/comatching/comatching3/notice/repository/NoticeConfirmRepository.java
@@ -0,0 +1,8 @@
+package comatching.comatching3.notice.repository;
+
+import comatching.comatching3.notice.domain.entity.NoticeConfirm;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface NoticeConfirmRepository extends JpaRepository<NoticeConfirm, Long> {
+
+}
