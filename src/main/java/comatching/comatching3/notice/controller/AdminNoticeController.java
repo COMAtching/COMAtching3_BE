352b95fccbfd96b4bbbfd5de8e5c25c256e37706
greensnapback0229 <smdmim@gmail.com>
2025-05-14 17:25:41 +0900

    feat($Notice): notice 취소, 조회 controller 추가

---
+++ src/main/java/comatching/comatching3/notice/controller/AdminNoticeController.java
@@ -0,0 +1,48 @@
+package comatching.comatching3.notice.controller;
+
+import comatching.comatching3.notice.dto.request.NoticeRegisterReq;
+import comatching.comatching3.notice.dto.response.NoticeRes;
+import comatching.comatching3.notice.service.AdminNoticeService;
+import comatching.comatching3.util.Response;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@Slf4j
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/auth/admin/notice")
+public class AdminNoticeController {
+
+    private final AdminNoticeService adminNoticeService;
+
+    @PostMapping
+    public Response addNotice(@RequestBody NoticeRegisterReq notice) {
+        adminNoticeService.addGeneralNotice(notice);
+
+        return Response.ok();
+    }
+
+
+    @DeleteMapping
+    public Response deleteNotice(@RequestParam("id") long id) {
+        adminNoticeService.cancelNotice(id);
+
+        return Response.ok();
+    }
+
+    @GetMapping
+    public Response getOpenNotice(@RequestParam String state) {
+        
+        if (state.equals("open")) {
+            List<NoticeRes> res = adminNoticeService.getOpenNotices();
+            return Response.ok(res);
+        } else if (state.equals("history")) {
+            List<NoticeRes> res = adminNoticeService.getClosedNotices();
+        }
+
+        return Response.ok();
+    }
+}
--- src/main/java/comatching/comatching3/notice/repository/NoticeRepository.java
+++ src/main/java/comatching/comatching3/notice/repository/NoticeRepository.java
@@ -1,8 +1,19 @@
 package comatching.comatching3.notice.repository;
 
 import comatching.comatching3.notice.domain.entity.Notice;
+import io.lettuce.core.dynamic.annotation.Param;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.time.LocalDateTime;
+import java.util.List;
 
 public interface NoticeRepository extends JpaRepository<Notice, Long> {
-    
+
+    @Query("SELECT n FROM Notice n WHERE n.closedAt > :now AND n.cancelled = false")
+    List<Notice> findOpenNotice(@Param("now") LocalDateTime now);
+
+    @Query("SELECT n FROM Notice n WHERE n.closedAt < :now AND n.cancelled = false")
+    List<Notice> findCloseNotice(@Param("now") LocalDateTime now);
+
 }
