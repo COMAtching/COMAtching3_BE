1b4eb296b3c57f027002e06dfdcdcba8e46fcccd
greensnapback0229 <smdmim@gmail.com>
2025-05-18 21:32:19 +0900

    build($Chat): 웹소켓 configure 추가

---
+++ src/main/java/comatching/comatching3/chat/config/WebSocketConfig.java
@@ -0,0 +1,23 @@
+package comatching.comatching3.chat.config;
+
+import comatching.comatching3.chat.CustomHttpHandshakeInterceptor;
+import comatching.comatching3.chat.handler.ChatWebSocketHandler;
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.socket.config.annotation.EnableWebSocket;
+import org.springframework.web.socket.config.annotation.WebSocketConfigurer;
+import org.springframework.web.socket.config.annotation.WebSocketHandlerRegistry;
+
+@Configuration
+@EnableWebSocket
+@RequiredArgsConstructor
+public class WebSocketConfig implements WebSocketConfigurer {
+    private final ChatWebSocketHandler chatWebSocketHandler;
+
+    @Override
+    public void registerWebSocketHandlers(WebSocketHandlerRegistry registry) {
+        registry.addHandler(chatWebSocketHandler, "/ws/chat")
+                .addInterceptors(new CustomHttpHandshakeInterceptor())
+                .setAllowedOrigins("*");
+    }
+}
