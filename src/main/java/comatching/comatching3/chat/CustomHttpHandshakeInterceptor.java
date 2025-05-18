1f1ac9d438f3b329da1095c46136786c19d85f56
greensnapback0229 <smdmim@gmail.com>
2025-05-18 21:36:15 +0900

    build($Chat): session 저장용 custom interceptor 추가

---
+++ src/main/java/comatching/comatching3/chat/CustomHttpHandshakeInterceptor.java
@@ -0,0 +1,29 @@
+package comatching.comatching3.chat;
+
+import jakarta.servlet.http.HttpSession;
+import org.springframework.http.server.ServerHttpRequest;
+import org.springframework.http.server.ServerHttpResponse;
+import org.springframework.http.server.ServletServerHttpRequest;
+import org.springframework.web.socket.WebSocketHandler;
+import org.springframework.web.socket.server.HandshakeInterceptor;
+
+import java.util.Map;
+
+public class CustomHttpHandshakeInterceptor implements HandshakeInterceptor {
+    @Override
+    public boolean beforeHandshake(ServerHttpRequest request, ServerHttpResponse response,
+                                   WebSocketHandler wsHandler, Map<String, Object> attributes) {
+        if (request instanceof ServletServerHttpRequest servletRequest) {
+            HttpSession session = servletRequest.getServletRequest().getSession(false);
+            if (session != null) {
+                attributes.put("HTTP_SESSION", session); // 실제 세션 저장
+            }
+        }
+        return true;
+    }
+
+    @Override
+    public void afterHandshake(ServerHttpRequest request, ServerHttpResponse response,
+                               WebSocketHandler wsHandler, Exception exception) {
+    }
+}
