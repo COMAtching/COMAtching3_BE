1478a8b296f8a8a86aee5567c18de6d0e554009c
greensnapback0229 <smdmim@gmail.com>
2025-05-20 13:24:19 +0900

    feat($Matching): Hobby 변경사항 매칭 내역 조회에 응답 dto 반영

--- src/main/java/comatching/comatching3/history/dto/res/MatchHistoryRes.java
+++ src/main/java/comatching/comatching3/history/dto/res/MatchHistoryRes.java
@@ -1,40 +1,38 @@
 package comatching.comatching3.history.dto.res;
 
-import java.util.List;
-
-import comatching.comatching3.users.entity.Hobby;
 import comatching.comatching3.users.entity.UserAiFeature;
 import comatching.comatching3.users.entity.Users;
 import comatching.comatching3.users.enums.ContactFrequency;
 import comatching.comatching3.users.enums.Gender;
-import comatching.comatching3.users.enums.HobbyEnum;
 import lombok.Getter;
+
+import java.util.List;
 
 @Getter
 public class MatchHistoryRes {
 
-	private String mbti;
-	private ContactFrequency contactFrequency;
-	private List<Hobby> hobbyList;
-	private Integer age;
-	private Gender gender;
-	private String  major;
-	private String comment;
-	private String song;
-	private String contactId;
+    private String mbti;
+    private ContactFrequency contactFrequency;
+    private List<String> hobbyList;
+    private Integer age;
+    private Gender gender;
+    private String major;
+    private String comment;
+    private String song;
+    private String contactId;
 
-	public void updateFromUsers(Users users){
-		UserAiFeature userAiFeature = users.getUserAiFeature();
-		this.mbti = userAiFeature.getMbti();
-		this.contactFrequency = userAiFeature.getContactFrequency();
-		this.hobbyList = userAiFeature.getHobbyList();
-		this.age = userAiFeature.getAge();
-		this.gender = userAiFeature.getGender();
-		this.major = userAiFeature.getMajor();
-		this.comment = users.getComment();
-		this.song = users.getSong();
-		this.contactId = users.getContactId();
-	}
+    public void updateFromUsers(Users users) {
+        UserAiFeature userAiFeature = users.getUserAiFeature();
+        this.mbti = userAiFeature.getMbti();
+        this.contactFrequency = userAiFeature.getContactFrequency();
+        this.hobbyList = userAiFeature.getHobbyNameList();
+        this.age = userAiFeature.getAge();
+        this.gender = userAiFeature.getGender();
+        this.major = userAiFeature.getMajor();
+        this.comment = users.getComment();
+        this.song = users.getSong();
+        this.contactId = users.getContactId();
+    }
 
 
 }
